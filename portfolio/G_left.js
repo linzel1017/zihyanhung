148581f968406320845672a6fa2dd29ddb4c2807
linzel1017 <55843031+linzel1017@users.noreply.github.com>
2023-06-02 01:46:15 +0800

    version1

---
+++ portfolio/G_left.js
@@ -0,0 +1,30 @@
+function navigateToTopPage() {
+}
+function showContent(contentId) {
+  var contents = document.getElementsByClassName('content');
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = 'none';
+  }
+  
+  var selectedContent = document.getElementById(contentId);
+  if (selectedContent) {
+    selectedContent.style.display = 'block';
+  }
+  
+  var rightSidebar = document.getElementById('rightSidebar');
+  rightSidebar.style.display = 'none';
+}
+
+function showImageInRightSidebar(imageUrl) {
+  var rightSidebar = document.getElementById('rightSidebar');
+  var rightImage = rightSidebar.querySelector('img');
+
+  rightImage.src = imageUrl;
+  rightSidebar.style.display = 'block';
+}
+
+function closeRightSidebar() {
+  var rightSidebar = document.getElementById('rightSidebar');
+  rightSidebar.style.display = 'none';
+}
+
